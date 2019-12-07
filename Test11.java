e8e0a9df678a48391f40864dfb68c1213bd0a16c
Artur Knul <artur.knul@gmail.com>
2019-12-07 11:40:02 +0000

    Test11.java

--- Test11.java
+++ Test11.java
@@ -3,6 +3,8 @@ public class Test11 {
     public static void main(String[] args) {
         for(String s : args) {
             System.out.println(s);
+			System.out.println(s);
+			System.out.println(s);
         }
     }
 }
